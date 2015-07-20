a99e35ab86c4d637131b95b3cfa68fc125e6964c
shangshin <shangshin@users.noreply.github.com>
2015-07-20 15:13:30 -0400

    Create HelloWorldTest.java

---
+++ HelloWorldTest.java
@@ -0,0 +1,20 @@
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import com.barclays.wps.elmo.HelloWorld;
+
+/**
+ * Run all junit tests
+ * @author Shangshin Liao
+ */
+
+public class HelloWorldTest extends junit.framework.TestCase {
+
+    public void testNothing() {
+    }
+
+    public void testWillAlwaysFail() {
+        fail("An error message");
+    }
+
+}
